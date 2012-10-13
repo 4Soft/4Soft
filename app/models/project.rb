d9b3a1d30871a73e796f5167bb3cc62cc080c1a4
painkiller778 <painkiller778@gmail.com>
2012-10-13 18:40:48 -0300

    Upando mais de uma imagem

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
   attr_accessible :name, :project
 
   has_one :cover
-  has_many :images, :as => :imageable, :class_name => "Image"
+  has_many :images, :as => :imageable, :class_name => "Image", :dependent => :destroy
 
   accepts_nested_attributes_for :images, :allow_destroy => true
 end
